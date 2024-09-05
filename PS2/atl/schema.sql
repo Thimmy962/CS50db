457ba440473ae12a11a0d8a2bad7a3fe53d4f53c
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-09-05 08:53:44 +0100

    atl assigment

--- PS2/atl/schema.sql
+++ PS2/atl/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Passengers (
-    "id" INTEGER NOT NULL,
+    "id" INTEGER,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
     "age" INTEGER NOT NULL,
@@ -7,4 +7,21 @@ CREATE TABLE IF NOT EXISTS Passengers (
 );
 
 
-CREATE TABLE IF NOT EXISTS 
+CREATE TABLE IF NOT EXISTS Airlines (
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    "concourse" TEXT NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS Flights (
+    "id" INTEGER,
+    "flight_number" INTEGER NOT NULL,
+    "airline_id" INTERGER NOT NULL,
+    "destination" TEXT NOT NULL CHECK(length(destination) = 3),
+    "departure" TEXT NOT NULL CHECK(length(departure) = 3),
+    "departure_time" NUMERIC NOT NULL,
+    "arrival_time" NUMERIC NOT NULL,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("airline_id") REFERENCES "airlines"("id")
+);
