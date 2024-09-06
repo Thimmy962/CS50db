79a4f4dc00745c4503cdcd86c272af7ba8aca306
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-09-06 10:08:03 +0100

    atl project for PS2

--- PS2/atl/schema.sql
+++ PS2/atl/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Passengers (
-    "id" INTEGER NOT NULL,
+    "id" SERIAL PRIMARY KEY,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
     "age" INTEGER NOT NULL,
@@ -7,4 +7,19 @@ CREATE TABLE IF NOT EXISTS Passengers (
 );
 
 
-CREATE TABLE IF NOT EXISTS 
+CREATE TABLE IF NOT EXISTS Airlines (
+    "id" SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "concourse" TEXT NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS Flights (
+    "id" SERIAL PRIMARY KEY,
+    "flight_number" INTEGER NOT NULL,
+    "airline_id" INTERGER  REFERENCES "airlines"("id") NOT NULL,
+    "destination" TEXT NOT NULL CHECK(length(destination) = 3),
+    "departure" TEXT NOT NULL CHECK(length(departure) = 3),
+    "departure_time" NUMERIC NOT NULL,
+    "arrival_time" NUMERIC NOT NULL
+);
